9a9090efffefea6b346667af730619fbd1475314
randaru09 <103346470+randaru09@users.noreply.github.com>
2022-10-06 22:13:26 +0700

    Add files via upload

---
+++ RC_Tugas2_Pemweb_120140019.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Tugas 2 Pemweb</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Website Pembelian Perlengkapan Olahraga</h1>
+    <div class="kotak">
+        <form action="action.php" method="get">
+            <p>Masukan jenis Barang :</p>
+            <p><input type="text" name="jenis"></p>
+            <p>Masukan Merk Barang : </p>
+            <p><input type="text" name="merk"></p>
+            <p><input type="submit"></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ action.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Website Pembelian Perlengkapan Olahraga</h1>
+    <div class="kotak">
+            <p><h3>Hasil Pencarian : </h3></p>
+            <p>Jenis Barang : <?=$_GET['jenis']?></p>
+            <p>Merk Barang : <?=$_GET['merk']?></p>
+    </div>   
+</body>
+</html>
